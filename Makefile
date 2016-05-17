fbeb9826dafc76bf46ace42996426e3f3d0511e7
animakozak <animakozak@ex.ua>
2016-05-17 23:12:40 +0300

    Points

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: clean All
 
 All:
-	@echo "----------Building project:[ Array_Insane_Session - Debug ]----------"
-	@cd "Array_Insane_Session" && "$(MAKE)" -f  "Array_Insane_Session.mk"
+	@echo "----------Building project:[ structures_points - Debug ]----------"
+	@cd "structures_points" && "$(MAKE)" -f  "structures_points.mk"
 clean:
-	@echo "----------Cleaning project:[ Array_Insane_Session - Debug ]----------"
-	@cd "Array_Insane_Session" && "$(MAKE)" -f  "Array_Insane_Session.mk" clean
+	@echo "----------Cleaning project:[ structures_points - Debug ]----------"
+	@cd "structures_points" && "$(MAKE)" -f  "structures_points.mk" clean
--- structures_points/points.cpp
+++ structures_points/points.cpp
@@ -48,46 +48,105 @@ void array134(){
         }
     }
     if(dist!=0) cout<<"FIRST maximum distance is "<<dist<<" between ("<< face[a].x <<";"<< face[a].y <<") and ("<< face[b].x << ";" << face[b].y <<")" <<endl;
-    else if(dis=0) cout<<"points are all the same"<<endl;
+    else if(dist==0) cout<<"points are all the same"<<endl;
 }
 void array136(){
     cout<<"Array 136"<<endl;
-    int dist=0;
+    int min_sum=99999;
     point face[N1];
+    int a=0;
+    int min_a=0;
+    int sum=0;
     cout<<"Fill the array"<<endl;
     for(int i=0; i<N1; i++){
         cout<<"x: ";
         cin>>face[i].x;
-        cout<<" y: ";
+        cout<<"y: ";
         cin>>face[i].y;
         cout<<endl;
     }
-    int min=0;
-    int a=0;
-    int b=0;
-    int min_a=0;
-    int min_b=0;
     for(int i=0; i<N1; i++){
+        sum=0;
         for(int j=0; j<N1; j++){
-            if(face[i].x!=face[j].x && face[i].y!=face[j].y){
-                if(point_distance(face[i], face[j])>max){
-                max=point_distance(face[i], face[j]);
-                max_a=i;
-                max_b=j;
-                }
-            }
+                sum=sum+point_distance(face[i], face[j]);
+                min_a=i;
         }
-        if(dist<max){
-            dist=max;
-            a=max_a;
-            b=max_b;
-            max=0;
-            max_a=0;
-            max_b=0;
+        if(min_sum>sum){
+            min_sum=sum;
+            a=min_a;
+            min_a=0;
         }
     }
-    if(dist!=0) cout<<"FIRST maximum distance is "<<dist<<" between ("<< face[a].x <<";"<< face[a].y <<") and ("<< face[b].x << ";" << face[b].y <<")" <<endl;
-    else if(dis=0) cout<<"points are all the same"<<endl;
+    if(min_sum!=0) cout<<"FIRST least isolated point is ("<< face[a].x <<";"<< face[a].y <<") with the sum of "<<min_sum<<endl;
+    else if(min_sum==0) cout<<"Well, that's strange :D"<<endl;
+}
+void array137(){
+    cout<<"Array 137"<<endl;
+    point face[N1];
+    double p_max=-1;
+    point A;
+    point B;
+    point C;
+    int i, j;
+    cout<<"Fill the array"<<endl;
+    for(int i=0; i<N1; i++){
+        cout<<"x: ";
+        cin>>face[i].x;
+        cout<<"y: ";
+        cin>>face[i].y;
+        cout<<endl;
+    }
+    for(i=0; i<N1; i++){
+        double p=0;
+        for(j=0; j<N1-1; j++){
+            if(i!=j)p=point_distance(face[i],face[j])+point_distance(face[j],face[j+1])+point_distance(face[j+1],face[i]);
+            if(p_max<p){
+            p_max=p;
+            if(i<=j){
+                A=face[i];
+                B=face[j];
+                C=face[j+1];
+            }
+            else if(i<=j+1 && i>j){
+                A=face[j];
+                B=face[i];
+                C=face[j+1];
+            }
+            else if(i>j+1){
+                A=face[j];
+                B=face[j+1];
+                C=face[i];
+            }
+        }
+        }
+        
+    }
+    cout<<"Max triangular P is "<<p_max<<" formed with the following vertex: A("<<A.x<<";"<<A.y<<") B("<<B.x<<";"<<B.y<<") C("<<C.x<<";"<<C.y<<")"<<endl;
+}
+void array140(){
+    cout<<"Array 140"<<endl;
+    point face[N1];
+    bool sorted=true;
+    cout<<"Fill the array"<<endl;
+    for(int i=0; i<N1; i++){
+        cout<<"x: ";
+        cin>>face[i].x;
+        cout<<"y: ";
+        cin>>face[i].y;
+        cout<<endl;
+    }
+    while(sorted){
+        sorted=false;
+            for(int i=0; i<N1-1; i++){
+            if((face[i].x+face[i].y<face[i+1].x+face[i+1].y)||((face[i].x+face[i].y==face[i+1].x+face[i+1].y)&&(face[i].x<face[i+1].x))){
+                swap(face[i],face[i+1]);
+                sorted=true;
+            }
+        }
+    }
+    for(int i=0; i<N1; i++){
+        cout<<" ("<<face[i].x<<";"<<face[i].y<<"), ";
+    }
 }
 int main(){
     home:
@@ -97,13 +156,15 @@ int main(){
     switch(k){
         case 134: array134(); break;
         case 136: array136(); break;
+        case 137: array137(); break;
+        case 140: array140(); break;
         default: cout<<"ERROR"<<endl;
     }
-    cout<<"Type in \"Home\" to go to the top, \"End\" to finish the program"<<endl;
+    cout<<"Type in \"home\" to go to the top, \"end\" to finish the program"<<endl;
     string exit;
     cin>>exit;
-    if (exit=="Home"){
+    if (exit=="home"){
         goto home;
     }
-    else return 0;
+    else if(exit=="end") return 0;
 }
